667e4344bd329ec1d4e016bc7f06882e5653501f
Sébastien Vidal <Sbastien Vidal>
2016-06-28 11:39:47 +0200

    réorganisation, intégration de gulp, corrections

--- tests/tests.js
+++ tests/tests.js
@@ -3,22 +3,22 @@
 
 // deps
 
-	const	assert = require('assert'),
-			SimpleTTS = require(require('path').join(__dirname, '..', 'main.js'));
+	const	assert = require("assert"),
+			SimpleTTS = require(require("path").join(__dirname, "..", "lib", "main.js"));
 
 // tests
 
-describe('get system type', function() {
+describe("get system type", function() {
 
-	it('should return a string', function() {
-		assert.strictEqual('string', typeof SimpleTTS.getTTSSystem(), "TTS system not returned as a string");
+	it("should return a string", function() {
+		assert.strictEqual("string", typeof SimpleTTS.getTTSSystem(), "TTS system not returned as a string");
 	});
 
 });
 
-describe('get voices', function() {
+describe("get voices", function() {
 
-	it('should return a voices array', function(done) {
+	it("should return a voices array", function(done) {
 
 		SimpleTTS.getVoices().then(function(voices) {
 			assert.strictEqual(true, voices instanceof Array, "voices are not returned as an array");
@@ -29,9 +29,9 @@ describe('get voices', function() {
 
 });
 
-describe('read', function() {
+describe("read", function() {
 
-	it('should play a text with options', function(done) {
+	it("should play a text with options", function(done) {
 
 		SimpleTTS.getVoices().then(function(voices) {
 			return SimpleTTS.read({text: "ceci est un test", voice: voices[0], speed: 70, volume: 30});
@@ -39,7 +39,7 @@ describe('read', function() {
 
 	}).timeout(4000);
 
-	it('should play a text without options', function(done) {
+	it("should play a text without options", function(done) {
 		SimpleTTS.read("ceci est un test").then(done).catch(done);
 	}).timeout(4000);
 
